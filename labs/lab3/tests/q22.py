ec3550b7c40ac0e77b737ada2878b7ecf13a08bf
Graham Alexander <gbalexan@ualberta.ca>
2022-01-30 23:24:16 -0700

    add lab3

---
+++ labs/lab3/tests/q22.py
@@ -0,0 +1,1 @@
+test = {'name': 'q22', 'points': None, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ labs/lab3/tests/q31.py
@@ -0,0 +1,1 @@
+test = {'name': 'q31', 'points': None, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
